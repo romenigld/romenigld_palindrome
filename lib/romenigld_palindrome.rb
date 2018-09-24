5e687bd97537c3093f38c0d383be613ced2af943
Romenig Lima Damasio <romenigld@gmail.com>
2018-09-24 12:35:44 +0200

    8.2) Initial test coverage

--- lib/romenigld_palindrome.rb
+++ lib/romenigld_palindrome.rb
@@ -1,5 +1,15 @@
 require "romenigld_palindrome/version"
 
-module RomenigldPalindrome
-  # Your code goes here...
+class String
+
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.downcase
+    end
 end
--- test/romenigld_palindrome_test.rb
+++ test/romenigld_palindrome_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
 class RomenigldPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::RomenigldPalindrome::VERSION
+  def test_non_palindrome
+    assert !"apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
   end
 end
