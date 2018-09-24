e31ef0346629153382a25518766638dbe4ad3b35
Romenig Lima Damasio <romenigld@gmail.com>
2018-09-24 22:19:17 +0200

    8.4) Green

--- lib/romenigld_palindrome.rb
+++ lib/romenigld_palindrome.rb
@@ -9,12 +9,19 @@ class String
 
   # Returns the letters in the string.
   def letters
+    the_letters = []
+    for i in 0..self.length - 1 do
+      if self[i].match(/[a-zA-Z]/)
+        the_letters << self[i]
+      end
+    end
+    the_letters.join
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.downcase
+      self.letters.downcase
     end
 end
