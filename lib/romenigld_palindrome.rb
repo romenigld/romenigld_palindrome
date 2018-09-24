4d13ec3fcc245940e0e229f154e3d20dbc90ca72
Romenig Lima Damasio <romenigld@gmail.com>
2018-09-24 23:28:57 +0200

    8.5) Refactor - Refactoring letters down to a single line with select.

--- lib/romenigld_palindrome.rb
+++ lib/romenigld_palindrome.rb
@@ -9,16 +9,7 @@ class String
 
   # Returns the letters in the string.
   def letters
-    the_letters = []
-    letter_regex = /[a-z]/i
-
-    self.chars.each do |character|
-      if character.match(letter_regex)
-        the_letters << character
-      end
-    end
-
-    the_letters.join
+    self.chars.select { |c| c.match(/[a-z]/i) }.join
   end
 
   private
